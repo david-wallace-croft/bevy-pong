fc6f111ac3119611db432dc0a61aafe1ffebe4b7
David Wallace Croft <david@CroftSoft.com>
2025-08-22 07:16:44 -0500

    hello world

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,11 @@ fn main() {
 
   let app: &mut App = app.add_plugins(DefaultPlugins);
 
+  let app: &mut App = app.add_systems(Startup, hello_world);
+
   let _app_exit: AppExit = app.run();
+}
+
+fn hello_world() {
+  println!("Hello, World!");
 }
